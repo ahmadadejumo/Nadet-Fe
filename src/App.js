bdb760ac10d1ea608af367483abedc13ad7da1d9
ahmadadejumo <adejumoahmad@gmail.com>
2021-12-29 16:54:05 +0100

    added the custom header color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import "./components/header/Header.js";
+import Header from "./components/header/Header.js";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
