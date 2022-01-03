03e90f5a75c08cedf7dd01aca83502a15ec192fe
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-03 09:44:26 +0100

    worked on the tablet responsive view of the discover component

--- src/components/discover/Discover.jsx
+++ src/components/discover/Discover.jsx
@@ -3,25 +3,25 @@ import macbook from "../../assets/images/macbook.png";
 
 const Discover = () => {
   return (
-    <div className="bg-hcolor">
-      <div className="mx-5 pt-16">
-        <img src={macbook} alt="macbook" />
-      </div>
-      <div className="pt-16 mx-5">
-        <h1 className="font-Lato font-bold text-2xl">
-          Get more discovered, and sell worldwide
-        </h1>
-      </div>
-      <div className="pt-5 mx-5">
-        <p className="font-Lato font-normal text-base leading-5 tracking-normal">
-          Put your products in front of thousands of customers that search
-          through our marketplace.
-        </p>
-      </div>
-      <div className="pt-5 mx-5 pb-14">
-        <button className="bg-bcolor font-Lato font-semibold h-12 w-56 rounded-lg tracking-normal">
-          Visit the Marketplace
-        </button>
+    <div className="bg-hcolor ">
+      <div className="md:flex md:flex-row-reverse mx-2">
+        <div className="mx-6 pt-16 md:pt-14">
+          <img src={macbook} alt="macbook" className="md:pl-10" />
+        </div>
+        <div className="mx-5 md:pt-5">
+          <h1 className="font-Lato font-bold text-2xl pt-16">
+            Get more discovered, and sell worldwide
+          </h1>
+          <p className="font-Lato font-normal text-tcolor text-base leading-5 tracking-normal pt-5">
+            Put your products in front of thousands of customers that search
+            through our marketplace.
+          </p>
+          <div className="pt-5 pb-14">
+            <button className="bg-bcolor font-Lato font-semibold h-12 w-56 rounded-lg tracking-normal">
+              Visit the Marketplace
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
