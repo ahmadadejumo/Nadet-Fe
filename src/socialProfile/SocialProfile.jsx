a20fa42610ec6c4e30e5bc4c449f7db88a7769b0
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-05 11:20:37 +0100

    worked on the tablet responsive view of the social profile section

--- src/socialProfile/SocialProfile.jsx
+++ src/socialProfile/SocialProfile.jsx
@@ -5,16 +5,16 @@ import profile2 from "../assets/images/profile2.png";
 const SocialProfile = () => {
   return (
     <div className="bg-[#FEF8E7]">
-      <div className="mx-16">
-        <div className="flex space-x-4 justify-center">
+      <div className="mx-16 md:flex md:mx-5 md:space-x-5 md:justify-center">
+        <div className="flex space-x-4 justify-center h-full w-full md:mt-[40px]">
           <img src={profile1} alt="profile" className="pt-[83px]" />
           <img src={profile2} alt="profile" className="pt-[50px] pb-9" />
         </div>
         <div className="mt-[60px]">
-          <h1 className="font-Lato font-bold text-2xl">
+          <h1 className="font-Lato font-bold md:text-[26px] text-2xl">
             Build a social profile
           </h1>
-          <p className="font-Lato font-normal text-base text-tcolor pt-5">
+          <p className="font-Lato font-normal text-tcolor pt-5">
             Invite your social media followers to follow your Nadet profile and
             notify them with a click of a button when you launch a product.
           </p>{" "}
