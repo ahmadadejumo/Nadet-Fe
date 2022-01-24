579e82b147afab55a9e61f9c819297488967214e
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-24 18:18:43 +0100

    add image in body section

--- src/pages/Events/Events.jsx
+++ src/pages/Events/Events.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import rectangle17 from "../../assets/images/rectangle17.png";
+import rectangle18 from "../../assets/images/rectangle18.png";
+import rectangle19 from "../../assets/images/rectangle19.png";
 
 const Events = () => {
   return (
@@ -15,14 +18,28 @@ const Events = () => {
           </button>
         </div>
       </div>
-      <div>
-        <div>
-          <div><img src={} alt="img" className="object-contain" /></div>
-          <div><img src={} alt="img" className="object-contain" /></div>
-          <div><img src={} alt="img" className="object-contain" /></div>
+      {/* Body Section */}
+      <div className="pt-[80px]">
+        <div className="flex justify-center space-x-2 px-5">
+          <div>
+            <img src={rectangle17} alt="img" className="object-contain" />
+          </div>
+          <div>
+            <img src={rectangle18} alt="img" className="object-contain" />
+          </div>
+          <div>
+            <img src={rectangle19} alt="img" className="object-contain" />
+          </div>
         </div>
-   </div>
-     
+        <h1 className="text-2xl font-bold text-center pt-[80px]">
+          Upcoming Events
+        </h1>
+        <p className="font-normal text-tcolor text-xl text-center px-5 pt-5">
+          Welcome to our event lists. Our meetup series typically includes
+          conversation with leaders, thought-provoking discussions and
+          networking opportunities.
+        </p>
+      </div>
     </div>
   );
 };
