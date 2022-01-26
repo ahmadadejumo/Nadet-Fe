62d42e7652cf7f41bc7a9a55a004295b1840a990
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-26 17:35:04 +0100

    done with terms of service page

--- src/pages/TermsOfService/TermsOfService.jsx
+++ src/pages/TermsOfService/TermsOfService.jsx
@@ -1,12 +1,316 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import YellowCard from "../../components/YellowCard";
 
 const TermsOfService = () => {
   return (
     <div className="font-Lato">
+      {/* Header section */}
       <div className="bg-[#FFF8E8]">
         <h1 className="font-extrabold text-2xl md:text-3xl lg:text-4xl text-center px-5 pt-[129px] pb-[50px]">
           Terms & Conditions
         </h1>
+      </div>
+      {/* Body section */}
+      <div className="px-5 pt-[80px] md:px-16 lg:px-[140px]">
+        <h1 className="font-bold text-2xl md:text-3xl">Introduction :</h1>
+        <p className="font-normal text-base text-tcolor pt-2">
+          By signing up for an account on this website{" "}
+          <Link to={"/"} className="text-bcolor">
+            (www.nadet.co)
+          </Link>
+          , and/or services, you are deemed a user and agree to these Merchant
+          Terms of Service (the “Agreement”).
+          <br />
+          <br />
+          PLEASE READ THESE MERCHANT TERMS OF SERVICE CAREFULLY BEFORE SIGNING
+          UP AS A MERCHANT. If you do not agree to any or all of these Terms of
+          Service, DO NOT USE THIS SITE!
+        </p>
+        <h1 className="pt-[40px] font-bold text-2xl md:text-3xl">About Us :</h1>
+        <p className="font-normal text-base text-tcolor pt-2">
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          (“we”, “us” or “our”) makes it easy for content creators to create,
+          market and sell online courses, ebooks and webinars.
+          <br />
+          <br />
+          We are an independent contractor for all purposes, providing this
+          website and our services on an independent service provider basis. We
+          do not endorse, have control or assume the liability or legality for
+          the products or services that are paid for with our service. We do not
+          guarantee any user’s identity and cannot ensure that a buyer or seller
+          will complete a transaction.
+        </p>
+        <h1 className="pt-10 font-bold text-2xl md:text-3xl">
+          Agreeing to These Terms & Conditions :
+        </h1>
+        <p className="font-normal text-base text-tcolor pt-2">
+          We offer a range of services depending on your needs. Individuals come
+          to{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          to both post and purchase content. A majority of these Terms and
+          Conditions will apply to both individuals and suppliers. In some
+          cases, the responsibilities of individuals purchasing content and
+          suppliers providing content vary. If these Terms and Conditions are
+          inconsistent with specific Service Terms, those Service Terms will
+          apply.
+          <br />
+          <br />
+          IMPORTANT – PLEASE CAREFULLY READ AND UNDERSTAND THESE TERMS AND
+          CONDITIONS OF USE & SALE (THESE “TERMS”) BEFORE ACCESSING, USING, OR
+          SUBSCRIBING OR PLACING AN ORDER OVER WWW.NADETACADEMY.ORG OR OTHER OF
+          OUR SITES OR ONLINE RESOURCES WHICH LINK TO THESE TERMS.
+          <br />
+          <br />
+          THESE TERMS CONTAIN DISCLAIMERS OF WARRANTIES AND LIMITATIONS OF
+          LIABILITIES INCLUDING ARBITRATION AND CLASS ACTION WAIVER PROVISIONS
+          THAT WAIVE YOUR RIGHT TO A COURT HEARING, RIGHT TO A JURY TRIAL, AND
+          RIGHT TO PARTICIPATE IN A CLASS ACTION (SEE SECTIONS 10, 14, 15, AND
+          16). ARBITRATION IS MANDATORY AND IS THE EXCLUSIVE REMEDY FOR ANY AND
+          ALL DISPUTES UNLESS SPECIFIED BELOW IN SECTION 16. THESE TERMS FORM AN
+          ESSENTIAL BASIS OF OUR AGREEMENT.
+        </p>
+        <h1 className="pt-10 font-bold text-2xl md:text-3xl">General Use :</h1>
+        <p className="font-normal text-base text-tcolor pt-2">
+          The use of{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          or online resources to which these Terms are linked (each, a
+          “Website”), owned and maintained by Nadet (“Nadet,” “we,” “our,”
+          “us”), are governed by these Terms. We offer the Website, including
+          all information, tools, and services available from the Website to
+          you, the user, conditioned upon your acceptance of all terms and
+          conditions stated herein. By accessing, using, subscribing, or placing
+          an order over the Website, you and your business (including any sub
+          users you may have) agree to the terms and conditions set forth
+          herein. If you do not agree to these Terms in their entirety, you are
+          not authorized to use the Website in any manner or form whatsoever.
+          <br />
+          <br />
+          THIS IS A BINDING AGREEMENT. THESE TERMS TOGETHER WITH OUR PRIVACY
+          STATEMENT AND DATA PROCESSING ADDENDUM AND CODE OF ETHICS FORM A
+          LEGALLY BINDING AGREEMENT (THE “AGREEMENT”) BETWEEN YOU AND YOUR
+          BUSINESS (“YOU”) AND NADETACADEMY. THIS AGREEMENT GOVERNS YOUR ACCESS
+          TO AND USE OF THE WEBSITE AND THE SERVICES PROVIDED BY NADETACADEMY,
+          ANY ORDER YOU PLACE THROUGH THE WEBSITE, BY TELEPHONE, OR OTHER
+          ACCEPTED METHOD OF PURCHASE AND, AS APPLICABLE, YOUR USE OR ATTEMPTED
+          USE OF THE PRODUCTS OR SERVICES OFFERED ON OR AVAILABLE THROUGH THE
+          WEBSITE.
+          <br />
+          <br />
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          reserves the right to update and change, from time to time, these
+          Terms and all documents incorporated by reference by posting updates
+          and/or changes to our Website. It is your responsibility to check this
+          page periodically for changes. You can find the most recent version of
+          these Terms here. Use of the Website after such changes constitutes
+          acceptance of such changes. Any new features or tools which are added
+          to the current Website shall also be subject to the Terms.
+        </p>
+        <h1 className="font-bold text-2xl md:text-3xl pt-10">Website Use :</h1>
+        <p className="font-normal text-base text-tcolor pt-2">
+          The Website is intended for businesses operated by adults. If you use
+          the Website, you are affirming that you are at least 18 years old or
+          the legal age of majority in your state or province of residence
+          (whichever is greater), operate a business, have the legal capacity to
+          enter into a binding contract with us, and have read this Agreement
+          and understand and agree to its terms.
+        </p>
+        <h1 className="pt-10 text-2xl md:text-3xl font-bold">
+          Information You Provide; Registration; Passwords :
+        </h1>
+        <p className="text-base text-tcolor pt-2 font-normal">
+          As a{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          user, you will be required to create an account with Nadet. You
+          warrant that the information you provide us is truthful and accurate,
+          and that you are not impersonating another person. You are responsible
+          for maintaining the confidentiality of any password you may use to
+          access your Nadet user account, and you agree not to transfer your
+          password or user name, or lend or otherwise transfer your use of or
+          access to your user account, to any third party. You are fully
+          responsible for all transactions with, and information conveyed to you
+          as Nadet Subscriber/User, including technical information, pricing,
+          business strategy, and data bout other past or current{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          users or their customers.
+        </p>
+        <h1 className="pt-10 text-2xl md:text-3xl font-bold">
+          Our Fees & Pricing Schedule :
+        </h1>
+        <p className="text-base text-tcolor pt-2 font-normal">
+          You agree to pay us for the services we render as a marketplace for
+          your goods and services. Our Fees will be calculated as demonstrated
+          on the Pricing page on the website. The Fees on our Pricing page is
+          integral to and forms part of this Agreement. We reserve the right to
+          revise our Fees. In the event that we revise our fees we will notify
+          you within 5 days of such change.
+        </p>
+        <h1 className="pt-10 text-2xl md:text-3xl font-bold">
+          Royalty payout :
+        </h1>
+        <p className="text-base text-tcolor pt-2 font-normal">
+          Potential users can pay by credit card or debit card. Payment details
+          shall be collected by us through our secure financial data collection
+          mechanism. You acknowledge and agree that we hold data relating to the
+          transaction, including the last four digits and the expiration date of
+          the card used to purchase the products or services together with
+          details on when payment is due. Subject to the terms of this
+          Agreement, Nadet.co will send to your designated bank or card
+          settlement account (“Bank Account”) all amounts settled and due to you
+          from your sells, minus our fees as stated in the Fee Schedule or other
+          amounts that you owe to Nadet.co under this Agreement (“Royalty
+          Payout”). Upon our request, you agree to provide us with all necessary
+          bank account and related information and grant us permission to debit
+          amounts due from your Bank Account.
+          <br />
+          <br />
+          After transfer of funds is initiated to your Bank Account, we will
+          update information on your Dashboard to reflect settlement. While we
+          will provide Transaction History in your Dashboard, you are solely
+          responsible for compiling and retaining permanent records of all
+          transactions and other data associated with your account as may be
+          required for your business. We are not responsible for maintaining
+          Transaction History or other records in a manner consistent with your
+          record retention obligations.
+          <br />
+          <br />
+          <Link to={"/"} className="text-bcolor">
+            Nadet
+          </Link>{" "}
+          reserves the right to immediately terminate a user’s account and/or
+          service for any unpaid (in whole or part) period of the subscription
+          (with or without notice). Termination of service in no way relieves or
+          excuses the user from any obligation to pay outstanding charges or
+          expenses. In the event{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet
+          </Link>{" "}
+          starts collection processes of any type, you will be liable for all
+          collection costs, including legal fees and expenses, as provided in
+          the terms of this agreement. In addition to any Fees,{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet
+          </Link>{" "}
+          may also charge applicable value added or other tax. To find out more
+          information about becoming a{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet
+          </Link>{" "}
+          Host and selling in our marketplace, as well as terms that apply
+          please{" "}
+          <Link to={"/"} className="text-bcolor">
+            click here
+          </Link>
+          . For avoidance of doubt, all Sections of these Terms apply to you in
+          your role as a user and as a Host, unless expressly provided
+          otherwise.
+        </p>
+        <h1 className="pt-10 text-2xl md:text-3xl font-bold">
+          Products, Services, and Prices Available on the Website :
+        </h1>
+        <p className="text-base text-tcolor pt-2 font-normal">
+          Products, services, and prices are generally posted at the following
+          URL, but are subject to change:
+          <Link to={"/"} className="text-bcolor">
+            www.Nadet.co
+          </Link>
+          . Price changes are effective on the first day of the month after the
+          price change is posted. By accessing, using, subscribing or placing an
+          order over the Website, you authorize{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          By your continued use of{" "}
+          <Link to={"/"} className="text-bcolor">
+            Nadet.co
+          </Link>{" "}
+          services, and unless you terminate your subscription as provided
+          herein, you agree that Nadet may charge your credit card monthly for
+          the products and services you have selected, and you consent to any
+          price changes for such services after e-mail notice has been provided
+          to you.
+          <br />
+          <br />
+          Nadet takes reasonable steps in an effort to ensure that the prices
+          set forth on the Website are correct, and to accurately describe and
+          display the items available on the Website. If the correct price of
+          our product is higher than its stated price, we will, at our
+          discretion, either contact you for instructions or cancel your order
+          and notify you of such cancellation.
+          <br />
+          <br />
+          When ordering products or services, please note that Nadet does not
+          warrant that product or service descriptions are accurate, complete,
+          current, or error-free, or that packaging will match the actual
+          product that you receive. All sales are deemed final except as
+          provided otherwise. Nadet ’s descriptions of, or references to,
+          products or services not owned by Nadet are not intended to imply
+          endorsement of that product or service or constitute a warranty by
+          Nadet.
+        </p>
+        <h1 className="pt-10 text-2xl md:text-3xl font-bold">Disclaimers :</h1>
+        <p className="text-base text-tcolor pt-2 font-normal">
+          We try to keep our services available at all times, bug-free and safe,
+          however, you use it at your own risk. Our website and services are
+          provided “as is” without any express, implied and/or statutory
+          warranties (including, but not limited to, any implied or statutory
+          warranties of merchantability, fitness for a particular use or
+          purpose, title, and non-infringement of intellectual property rights).
+          Without limiting the generality of the foregoing, we make no warranty
+          that our website and services will meet your requirements or that our
+          website will be uninterrupted, timely, secure, or error free. No
+          advice or information, whether oral or written, obtained by you
+          through our website or from nadet, its parents, subsidiaries, or other
+          affiliated companies, or its or their suppliers (or the respective
+          officers, directors, employees, or agents of any such entities)
+          (collectively, "Nadet parties") shall create any warranty.
+          <br />
+          <br />
+          Your Business’ Individual Results Will Vary Every online business is
+          different, employing different strategic approaches and organizational
+          structures, and offering different products and services. Therefore,
+          individual results will vary from user to user. YOUR BUSINESS’
+          INDIVIDUAL RESULTS WILL VARY DEPENDING UPON A VARIETY OF FACTORS
+          UNIQUE TO YOUR BUSINESS, INCLUDING BUT NOT LIMITED TO YOUR CONTENT,
+          BUSINESS MODEL, AND PRODUCT AND SERVICE OFFERINGS.
+          <br />
+          <br />
+          We do not promise, guarantee, or warrant your business’ success,
+          income, or sales. Those businesses who purchase our products or
+          services will receive access to software and tools to create Internet
+          sales funnels and otherwise assist with their respective online
+          offerings. However, we do not guarantee your business’ success and
+          based upon many market factors that we cannot control, the software
+          and tools we provide may or may not be applicable to your specific
+          business. Further, we do not make earnings claims, efforts claims
+          return on investment claims, or claims that our software, tools, or
+          other offerings will make your business any specific amount of money,
+          and it is possible that you will not earn your investment back. We do
+          not sell a business opportunity, “get rich quick” program, guaranteed
+          system, franchise system, or a business in a box. You should not
+          purchase our products or services if that is your expectation.
+          Instead, you should purchase with the understanding that using the
+          information and software purchased will take time and effort and may
+          be applicable in some situations but not others. Also, we do not offer
+          any tax, accounting, financial, or legal advice. You should consult
+          your business’ accountant, attorney, or financial advisor for advice
+          on these topics.
+        </p>
+      </div>
+      <div className="py-[80px] lg:pb-[120px]">
+        <YellowCard />
       </div>
     </div>
   );
