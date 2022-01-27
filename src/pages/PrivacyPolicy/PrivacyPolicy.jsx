31f510380d4360d84022846364215fb319b46431
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-27 18:20:13 +0100

    add body section

--- src/pages/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/pages/PrivacyPolicy/PrivacyPolicy.jsx
@@ -65,6 +65,216 @@ const PrivacyPolicy = () => {
           to this Privacy Policy, the User must cease using this Site, or
           terminate their account in the event an account has been created.
         </p>
+        <h1 className="font-bold text-2xl md:text-3xl">Age Restriction :</h1>
+        <p className="pt-2 text-base text-tcolor font-normal">
+          Our website and services are not directed to children under 18. We do
+          not knowingly collect information from children under 18. If as a
+          parent or guardian, you become aware that your child or ward child has
+          provided us with any information without your consent, please contact
+          us through the details provided in this Privacy Policy.
+        </p>
+        <h1 className="font-bold text-2xl md:text-3xl">
+          The Information we Collect :
+        </h1>
+        <div className="px-5">
+          <ul className="list-disc pt-5">
+            <li className="font-bold text-xl text-tcolor">
+              Personal Information
+            </li>
+            <p className="pt-2 text-base text-tcolor font-normal">
+              Our website and services are not directed to children under 18. As
+              part of our operations, Nadet collects and processes certain types
+              of information (such as name, telephone numbers, email address
+              etc.) of individuals that makes them easily identifiable. These
+              individuals include current, past and prospective employees,
+              content creators, customers of customers, Users and other
+              individuals whom Nadet communicates or deals with, jointly and/or
+              severally (“Data Subject(s)”). Nadet is firmly committed to
+              complying with applicable data protection laws, regulations, rules
+              and principles to ensure security of Personal Information handled
+              by the Company.
+              <br />
+              <br />
+              To gain full access to our website and services, you must register
+              for a Nadet account. When you register for an account, we collect
+              Personal Information which you voluntarily provide to us. Personal
+              Information refers to information relating to an identified person
+              or information that can be used to identify you, (e.g. email
+              address, bank details, name, telephone number). It may also
+              include anonymous information that may be linked to you
+              specifically, (e.g. IP address).
+              <br />
+              <br />
+              We use your Personal Information to:
+            </p>
+            <ul className="list-inside list-decimal">
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Provide you with the required services.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Respond to your questions or requests.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Improve features, website content and analyze data to develop
+                products and services.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Address inappropriate use of our website.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Prevent, detect and manage risk against fraud and illegal
+                activities using internal and third party screening tools.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Send you marketing content, newsletters and service updates
+                curated by Nadet , however, we will provide you with an option
+                to unsubscribe if you do not want to hear from us.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Verify your identity and the information you provide in line
+                with Nadet ’s statutory obligations using internal and third
+                party tools.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Maintain up-to-date records.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Resolve disputes that may arise, including investigations by law
+                enforcement or regulatory bodies.
+              </li>
+              <li className="pt-2 text-base text-tcolor font-normal">
+                Any other purpose that we disclose to you in the course of
+                providing our services to you.
+              </li>
+            </ul>
+            <p className="pt-5 text-base text-tcolor font-normal">
+              We may retrieve additional Personal Information about you from
+              third parties and other identification/verification services such
+              as your financial institution and payment processor. With your
+              consent, we may also collect additional Personal Information in
+              other ways including emails, surveys, and other forms of
+              communication.
+              <br />
+              <br />
+              Once you begin using our services through your Nadet account we
+              will keep records of your transactions and collect information of
+              your other activities related to our services. We will not share
+              or disclose your Personal Information with a third party without
+              your consent except as may be required for the purpose of
+              providing you with our services or under applicable legislations.
+              <br />
+              <br />
+              In providing you with the services, we may rely on third-party
+              servers located in foreign jurisdictions from time to time, which
+              as a result, may require the transfer or maintenance of your
+              personally identifiable information on computers or servers in
+              foreign jurisdictions. We will endeavour to ensure that such
+              foreign jurisdictions have data protection legislation that is no
+              less than the existing data protection regulations in force in
+              Nigeria and your personally identifiable information is treated in
+              a safe and secure manner.
+            </p>
+            <li className="font-bold text-xl text-tcolor pt-5">
+              Information that we collect from website visitors
+            </li>
+            <p className="pt-5 text-base text-tcolor font-normal">
+              We do not collect your Personal Information when you visit the
+              website. However, so we can monitor and improve our website and
+              services we may collect non-personally-identifiable information.
+              We will not share or disclose this information with third parties
+              except as a necessary part of providing our website and services.
+              We may use the information to target advertisements to you.
+            </p>
+            <li className="font-bold text-xl text-tcolor pt-5">
+              Purpose Limitation
+            </li>
+            <p className="pt-5 text-base text-tcolor font-normal">
+              Nadet collects Personal Information only for identified purposes
+              and for which consent has been obtained. Such Personal Information
+              cannot be reused for another purpose that is incompatible with the
+              original purpose, except consent is obtained for such purpose
+            </p>
+            <li className="font-bold text-xl text-tcolor pt-5">
+              Data Minimization
+            </li>
+            <p className="pt-5 text-base text-tcolor font-normal">
+              Nadet limits Personal Information collection and usage to data
+              that is relevant, adequate, and absolutely necessary for carrying
+              out the purpose for which the data is processed.
+              <br />
+              <br />
+              Nadet will evaluate whether and to what extent the processing of
+              Personal Information is necessary and where the purpose allows,
+              anonymized data will be used.
+            </p>
+          </ul>
+        </div>
+        <h1 className="font-bold text-2xl md:text-3xl pt-5">Cookies :</h1>
+        <p className="pt-2 text-base text-tcolor font-normal">
+          We use cookies to identify you as a User and make your user experience
+          easier, customize our services, content and advertising; help you
+          ensure that your account security is not compromised, mitigate risk
+          and prevent fraud; and to promote trust and safety on our website.
+          Cookies allow our servers to remember IP addresses, date and time of
+          visits, monitor web traffic and prevent fraudulent activities. Our
+          cookies never store personal or sensitive information; they simply
+          hold a unique random reference to you so that once you visit the site
+          we can recognize who you are and provide certain content to you.
+          <br />
+          <br />
+          If your browser or browser add-on permits, you have the choice to
+          disable cookies on our website; however this may impact your
+          experience using our website.
+        </p>
+        <h1 className="font-bold text-2xl md:text-3xl pt-5">
+          How We Protect your Information :
+        </h1>
+        <p className="pt-2 text-base text-tcolor font-normal">
+          Nadet shall establish adequate controls in order to protect the
+          integrity and confidentiality of Personal Information, both in digital
+          and physical format and to prevent Personal Information from being
+          accidentally or deliberately compromised.
+          <br />
+          <br />
+          Nadet.co is committed to managing your Personal Information in line
+          with global industry best practices. We protect your Personal
+          Information using physical, technical, and administrative security
+          measures to reduce the risks of loss, misuse, unauthorized access,
+          disclosure and alteration, we also use industry recommended security
+          protocols to safeguard your Personal Information. Other security
+          safeguards include but are not limited to data encryption, firewalls,
+          and physical access controls to our building and files and only
+          granting access to Personal Information to only employees who require
+          it to fulfill their job responsibilities. Any Personal Information
+          processing undertaken by an employee who has not been authorized to
+          carry such out as part of their legitimate duties is unauthorized.
+          <br />
+          <br />
+          Employees may have access to Personal Information only as is
+          appropriate for the type and scope of the task in question and are
+          forbidden to use Personal Information for their own private or
+          commercial purposes or to disclose them to unauthorized persons, or to
+          make them available in any other way.
+        </p>
+        <h1 className="font-bold text-2xl md:text-3xl pt-5">
+          How we share the Personal Information you provide :
+        </h1>
+        <p className="pt-2 text-base text-tcolor font-normal">
+          Nadet.co does not sell, trade or rent personal information to anyone.
+          However, to enable us render our services to you on our website, we
+          may share your information with trusted third parties, such third
+          parties include financial institutions, payment processors
+          verification services, sanctions screening and identity verification
+          services as well as any third parties that you have directly
+          authorized to receive your Personal Information. Your Personal
+          Information may be stored in locations outside the direct control of
+          Nadet, for instance, on servers or databases co-located with hosting
+          providers.
+          <br />
+          <br />
+          We may disclose your Personal Information in compliance with
+          applicable law or a legal obligation to which we are bound.
+        </p>
       </div>
     </div>
   );
