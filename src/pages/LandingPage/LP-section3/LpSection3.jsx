fec3d6c0dbc0bc4622aa48fc54b045c5f68a1fae
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-31 00:08:54 +0100

    fixed a bug

--- src/pages/LandingPage/LP-section3/LpSection3.jsx
+++ src/pages/LandingPage/LP-section3/LpSection3.jsx
@@ -3,25 +3,25 @@ import macbook from "../../../assets/images/macbook.png";
 
 const LpSection3 = () => {
   return (
-    <div className="bg-hcolor ">
-      <div className="md:flex md:flex-row-reverse mx-2 lg:mx-7 md:pb-10 lg:pb-5">
-        <div className="mx-6 pt-16 md:pt-14">
-          <img src={macbook} alt="macbook" className="md:pl-10" />
-        </div>
-        <div className="mx-5 md:pt-5 lg:pt-44">
-          <h1 className="font-Lato font-bold text-2xl lg:text-4xl pt-16">
-            Get more discovered, and sell worldwide
-          </h1>
-          <p className="font-Lato font-normal text-tcolor text-base leading-5 tracking-normal pt-5">
-            Put your products in front of thousands of customers that search
-            through our marketplace.
-          </p>
-          <div className="pt-5 pb-14">
-            <button className="bg-bcolor font-Lato font-semibold h-12 w-56 rounded-lg tracking-normal">
-              Visit the Marketplace
-            </button>
-          </div>
-        </div>
+    <div className="md:flex md:flex-row-reverse md:justify-center md:items-center lg:px-[140px] bg-hcolor">
+      <div className="flex justify-center pt-[60px] md:pt-14 px-5 lg:pt-[26px] lg:pb-[28px] md:pb-10">
+        <img
+          src={macbook}
+          alt="macbook"
+          className="object-contain h-[273px] w-[326px] md:w-[360px] md:h-[300px] lg:h-[545px] lg:w-[652px]"
+        />
+      </div>
+      <div className="mx-5 md:pt-[91px] pt-[61px] md:w-[283px] lg:w-[450px]">
+        <h1 className="font-Lato font-bold text-2xl md:text-[26px] lg:text-4xl leading-normal">
+          Get more discovered, and sell worldwide
+        </h1>
+        <p className="font-Lato font-normal text-tcolor text-base leading-normal pt-5">
+          Put your products in front of thousands of customers that search
+          through our marketplace.
+        </p>
+        <button className="bg-bcolor mt-5 mb-14 font-Lato font-semibold h-12 w-56 rounded-lg tracking-normal">
+          Visit the Marketplace
+        </button>
       </div>
     </div>
   );
