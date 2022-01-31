8c962d3a19fa787f1efbd74f8783898f464c054f
ahmadadejumo <adejumoahmad@gmail.com>
2022-01-31 16:58:26 +0100

    fixed a bug in landing page sec 7

--- src/pages/LandingPage/LP-section7/LpSection7.jsx
+++ src/pages/LandingPage/LP-section7/LpSection7.jsx
@@ -4,54 +4,61 @@ import image5 from "../../../assets/images/image5.png";
 
 const LpSection7 = () => {
   return (
-    <div className="mt-[40px]">
-      <div className="mx-5">
-        <h1 className="font-Lato font-bold text-2xl text-center lg:mx-96">
-          We give you the right tools to make more sales with ease
-        </h1>
-        <div className="container rounded-lg w-full bg-[#F5F4FD] mt-[40px]">
-          <div className="md:flex mx-5 md:mx-10 md:space-x-16 lg:mx-[65px]">
-            <div className="md:w-[450px] lg:w-[1200px] lg:mt-[31px]">
-              <h1 className="font-Lato font-bold text-2xl md:text-[26px] lg:text-4xl text-[#9B8EEB] pt-[50px] leading-9">
-                Refer your buddies to our platform and earn more money.
-              </h1>
-              <p className="font-Lato font-normal text-base pt-5 text-tcolor">
-                Our affilaite program helps you make more sales as you can
-                automatically pay commissions to people to help you promote your
-                contents.
-              </p>
-              <button className="bg-[#9B8EEB] font-Lato font-semibold rounded-lg h-[50px] w-[142px] mt-5 mb-[63px]">
-                Get Started
-              </button>
-            </div>
-            <div className="w-full md:mt-[90px] lg:mt-[50px]">
-              <img src={image5} alt="" className="w-full pb-[51px]" />
-            </div>
+    <div className="mt-[40px] mx-5 md:mx-[30px]">
+      <h1 className="font-Lato font-bold text-2xl md:text-[26px] lg:text-4xl text-center md:mx-[120px] lg:mx-[250px]">
+        We give you the right tools to make more sales with ease
+      </h1>
+      {/* First card */}
+      <div className="rounded-lg bg-[#F5F4FD] mt-[40px] lg:mx-[70px]">
+        <div className="md:flex mx-5 md:mx-[30px] lg:px-[10px] items-center md:space-x-[37px] lg:space-x-[83px]">
+          <div className="md:w-[296px] lg:w-[536px] mt-[50px] md:mt-[23px] lg:mt-[31px]">
+            <h1 className="font-Lato font-bold text-2xl md:text-[26px] lg:text-4xl text-[#9B8EEB] leading-relaxed">
+              Refer your buddies to our platform and earn more money.
+            </h1>
+            <p className="font-Lato font-normal text-base pt-5 text-tcolor">
+              Our affilaite program helps you make more sales as you can
+              automatically pay commissions to people to help you promote your
+              contents.
+            </p>
+            <button className="bg-[#9B8EEB] font-Lato font-semibold rounded-lg h-[50px] w-[142px] mt-5 mb-[63px] md:mb-[29px]">
+              Get Started
+            </button>
           </div>
-        </div>
-        {/* two seperate cards */}
-        <div className="container rounded-lg w-full bg-[#FDFAF2] mt-5">
-          <div className="md:flex md:flex-row-reverse mx-5 md:mx-10 lg:mx-[65px]">
-            <div className="md:w-[450px] lg:w-[1200px] pt-10">
-              <h1 className="font-Lato font-bold text-2xl md:text-[26px] lg:text-4xl text-[#FBBC15] leading-9">
-                Keep receiving and never lose an international customer again!
-              </h1>
-              <p className="font-Lato font-normal text-base pt-5 text-tcolor">
-                We've integrated multiple payment gateways in place to ensure
-                you can receive payments from anywhere in the world and you
-                don’t have to worry again.
-              </p>
-              <button className="bg-[#FBBC15] font-Lato font-semibold rounded-lg h-[50px] w-[142px] mt-5 mb-[63px]">
-                Learn More
-              </button>
-            </div>
-            <div className="w-full md:mt-[90px] lg:mt-[50px] md:mr-14">
-              <img src={image6} alt="" className="w-full pb-[51px]" />
-            </div>
+          <div className="pb-[51px] md:pb-px">
+            <img
+              src={image5}
+              alt=""
+              className="object-contain md:h-[167px] md:w-[315px] lg:w-[411px] lg:h-[219px]"
+            />
           </div>
         </div>
       </div>
-      <div className="flex justify-center mt-[50px]">
+      {/* second card */}
+      <div className="rounded-lg bg-[#FDFAF2] mt-5 md:mt-[40px] lg:mt-[60px] lg:mx-[70px]">
+        <div className="md:flex items-center mx-5 md:mx-[30px] lg:px-[10px] md:space-x-[25px] lg:space-x-[64px]">
+          <div className="pb-[51px] md:pb-px">
+            <img
+              src={image6}
+              alt=""
+              className="object-contain md:w-[315px] md:h-[167px] lg:w-[411px] lg:h-[219px]"
+            />
+          </div>
+          <div className="md:w-[328px] lg:w-[554px] pt-[50px] md:pt-[10px] lg:pt-[32px]">
+            <h1 className="font-Lato font-bold text-2xl md:text-[26px] lg:text-4xl text-[#FBBC15] leading-relaxed">
+              Keep receiving and never lose an international customer again!
+            </h1>
+            <p className="font-Lato font-normal text-base pt-5 text-tcolor">
+              We've integrated multiple payment gateways in place to ensure you
+              can receive payments from anywhere in the world and you don’t have
+              to worry again.
+            </p>
+            <button className="bg-[#FBBC15] font-Lato font-semibold rounded-lg h-[50px] w-[142px] mt-5 mb-[63px] md:mb-[15px] lg:mb-[32px]">
+              Get Started
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center mt-[40px]">
         <button className="bg-bcolor font-Lato font-semibold rounded-lg h-[50px] w-[199px] mt-5 mb-[54px]">
           Get Started Today!
         </button>
