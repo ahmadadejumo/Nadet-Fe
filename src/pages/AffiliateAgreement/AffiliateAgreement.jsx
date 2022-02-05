eeadc6d23b2c2d98b51d9445439dbb2f66ce59de
ahmadadejumo <adejumoahmad@gmail.com>
2022-02-05 12:49:16 +0100

    done with the affiliate agreement page

--- src/pages/AffiliateAgreement/AffiliateAgreement.jsx
+++ src/pages/AffiliateAgreement/AffiliateAgreement.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import YellowCard from "../../components/YellowCard";
 
 const AffiliateAgreement = () => {
   return (
     <div className="font-Lato">
       {/* Header section */}
-      <div className="bg-[#FFF8E8] px-5 pt-[129px] pb-[33px]">
+      <div className="bg-[#FFF8E8] px-5 md:px-36 lg:px-80 pt-[129px] pb-[33px]">
         <h1 className="font-extrabold text-[28px] md:text-3xl lg:text-4xl text-center">
           Nadet Academy Affiliate Program Terms of Service
         </h1>
       </div>
       {/* Body section */}
-      <div className="pt-[80px] px-5">
-        <h1 className="text-2xl font-bold">Agreement :</h1>
+      <div className="pt-[80px] px-5 md:px-10 lg:px-[140px]">
+        <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+          Agreement :
+        </h1>
         <p className="pt-5 text-base font-normal text-tcolor">
           By signing up to be an Affiliate in the Nadet Academy Affiliate
           Program (“Program”) you are agreeing to be bound by the following
@@ -34,6 +37,270 @@ const AffiliateAgreement = () => {
           <span className="font-bold text-black">Note:</span> You agree to use
           the Affiliate Program at your own risk.
         </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Account Terms :
+        </h1>
+        <ul className="list-disc list-inside">
+          <li className="pt-2 text-base font-normal text-tcolor">
+            You must be 18 years or older to be part of this Program.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            You must be a human. Accounts registered by “bots” or other
+            automated methods are not permitted.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            You must provide your legal full name, a valid email address, and
+            any other information requested in order to complete the signup
+            process.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            Your login may only be used by one person, a single login shared by
+            multiple people is not permitted.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            You are responsible for maintaining the security of your account and
+            password. Nadet Academy cannot and will not be liable for any loss
+            or damage from your failure to comply with this security obligation.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            You are responsible for all the Contents posted and activity that
+            occurs under your account.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            One person or legal entity may not maintain more than one account.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            You may not use the Affiliate Program for any illegal or
+            unauthorized purpose.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            You must not, in the use of the Service, violate any laws in your
+            jurisdiction (including but not limited to copyright laws).
+          </li>
+        </ul>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Links/graphics on your site, in your emails, or other communications :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          Once you have signed up for the Affiliate Program, you will be
+          assigned a unique Affiliate Code. You are permitted to place links,
+          banners, or other graphics we provide with your Affiliate Code on your
+          site, in your emails, or in other communications. We will provide you
+          with guidelines, link styles, and graphical artwork to use in linking
+          to Nadet Academy. We may change the design of the artwork at any time
+          without notice, but we won’t change the dimensions of the images
+          without proper notice.
+          <br />
+          <br />
+          To permit accurate tracking, reporting, and referral fee accrual, we
+          will provide you with special link formats to be used in all links
+          between your site and the Nadet Academy. You must ensure that each of
+          the links between your site and the Nadet Academy properly utilizes
+          such special link formats. Links to the Nadet Academy placed on your
+          site pursuant to this Agreement and which properly utilize such
+          special link formats are referred to as “Special Links.” You will earn
+          affiliate referral fees only with respect to sales on a Nadet Academy
+          product occurring directly through your Special Links; we will not be
+          liable to you with respect to any failure by you or someone you refer
+          to use Special Links or incorrectly type your Affiliate Code,
+          including to the extent that such failure may result in any reduction
+          of amounts that would otherwise be paid to you pursuant to this
+          Agreement.
+          <br />
+          <br />
+          <span className="font-bold text-black">Note:</span> Affiliate links
+          should point to the page of the product being promoted.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Referral fees/commissions and payment :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          For a Product sale to be eligible to earn an affiliate referral fee,
+          the customer must click-through a Special Link from your site, email,
+          or other communications to "https://nadet.co" and complete an order
+          for a video course, ebook, live class, audio lessons etc during that
+          session. We will only pay commissions on links that are automatically
+          tracked and reported by our systems. We will not pay commissions if
+          someone says they purchased or someone says they entered a referral
+          code if it was not tracked by our system. We can only pay commissions
+          on business generated through properly formatted special links that
+          were automatically tracked by our systems.
+          <br />
+          <br />
+          We reserve the right to disqualify commissions earned through
+          fraudulent, illegal, or overly aggressive, questionable sales or
+          marketing methods.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Payment schedule :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          Once you have started making money, you can request a payout and your
+          money will be automatically sent directly to your Bank account or
+          PayPal account. It's that easy!
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Customer definition :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          Customers who buy products through this Program will be deemed to be
+          our customers. Accordingly, all of our rules, policies, and operating
+          procedures concerning customer orders, customer service, and product
+          sales will apply to those customers. We may change our policies and
+          operating procedures at any time. For example, we will determine the
+          prices to be charged for products sold under this Program in
+          accordance with our own pricing policies. Product prices and
+          availability may vary from time to time. Because price changes may
+          affect Products that you have listed on your site, you should not
+          display product prices on your site. We will use commercially
+          reasonable efforts to present accurate information, but we cannot
+          guarantee the availability or price of any particular product.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Your responsibilities :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          You will be solely responsible for the development, operation, and
+          maintenance of your site and for all materials that appear on your
+          site. For example, you will be solely responsible for:
+        </p>
+        <ul className="list-disc list-inside">
+          <li className="pt-2 text-base font-normal text-tcolor">
+            The technical operation of your site and all related equipment.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            Ensuring the display of Special Links on your site does not violate
+            any agreement between you and any third party (including without
+            limitation any restrictions or requirements placed on you by a third
+            party that hosts your site).
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            The accuracy, truth, and appropriateness of materials posted on your
+            site (including, among other things, all Product-related materials
+            and any information you include within or associate with Special
+            Links).
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            Ensuring that materials posted on your site do not violate or
+            infringe upon the rights of any third party (including, for example,
+            copyrights, trademarks, privacy, or other personal or proprietary
+            rights).
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            Ensuring that materials posted on your site are not libelous or
+            otherwise illegal.
+          </li>
+          <li className="pt-2 text-base font-normal text-tcolor">
+            Ensuring that your site accurately and adequately discloses, either
+            through a privacy policy or otherwise, how you collect, use, store,
+            and disclose data collected from visitors, including, where
+            applicable, that third parties (including advertisers) may serve
+            content and/or advertisements and collect information directly from
+            visitors and may place or recognize cookies on visitors’ browsers.
+          </li>
+        </ul>
+        <h1>Compliance with Laws :</h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          As a condition to your participation in the Program, you agree that
+          while you are a Program participant you will comply with all laws,
+          ordinances, rules, regulations, orders, licenses, permits, judgments,
+          decisions or other requirements of any governmental authority that has
+          jurisdiction over you, whether those laws, etc. are now in effect or
+          later come into effect during the time you are a Program participant.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Term of the Agreement and Program :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          The term of this Agreement will begin upon our acceptance of your
+          Program application and will end when terminated by either party.
+          Either you or we may terminate this Agreement at any time, with or
+          without cause, by giving the other party written notice of
+          termination. Upon the termination of this Agreement for any reason,
+          you will immediately cease use of, and remove from your site, all
+          links to "https://nadet.co", and all of our trademarks, trade dress,
+          and logos, and all other materials provided by or on behalf of us to
+          you pursuant hereto or in connection with the Program. Nadet Academy
+          reserves the right to end the Program at any time.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Termination :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          Nadet Academy, in its sole discretion, has the right to suspend or
+          terminate your account and refuse any and all current or future use of
+          the Program, or any other Nadet Academy service, for any reason at any
+          time. Such termination of the Service will result in the deactivation
+          or deletion of your Account or your access to your Account, and the
+          forfeiture and relinquishment of all potential or to-be-paid
+          commissions in your Account if they were earned through fraudulent,
+          illegal, or overly aggressive, questionable sales or marketing
+          methods. Nadet Academy reserves the right to refuse service to anyone
+          for any reason at any time.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Limitations of Liability :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          We will not be liable for ind className="pt-5 text-base font-normal
+          text-tcolor"irect, special, or consequential damages (or any loss of
+          revenue, profits, or data) arising in connection with this Agreement
+          or the Program, even if we have been advised of the possibility of
+          such damages. Further, our aggregate liability arising with respect to
+          this Agreement and the Program will not exceed the total referral fees
+          paid or payable to you under this Agreement.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Disclaimers :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          We make no express or implied warranties or representations with
+          respect to the Program or any products sold through the Program
+          (including, without limitation, warranties of fitness,
+          merchantability, non-infringement, or any implied warranties arising
+          out of a course of performance, dealing, or trade usage). In addition,
+          we make no representation that the operation of the Nadet Academy will
+          be uninterrupted or error-free, and we will not be liable for the
+          consequences of any interruptions or errors.
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Miscellaneous :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          You may not assign this Agreement, by operation of law or otherwise,
+          without our prior written consent. Subject to that restriction, this
+          Agreement will be binding on, inure to the benefit of, and be
+          enforceable against the parties and their respective successors and
+          assigns. Our failure to enforce your strict performance of any
+          provision of this Agreement will not constitute a waiver of our right
+          to subsequently enforce such provision or any other provision of this
+          Agreement.
+          <br />
+          <br />
+          The failure of Nadet Academy to exercise or enforce any right or
+          provision of the Terms of Service shall not constitute a waiver of
+          such right or provision. The Terms of Service constitutes the entire
+          agreement between you and Nadet Academy and govern your use of the
+          Service, superseding any prior agreements between you and Nadet
+          Academy (including, but not limited to, any prior versions of the
+          Terms of Service).
+        </p>
+        <h1 className="pt-5 text-2xl font-bold md:text-3xl lg:text-4xl">
+          Independent Investigation :
+        </h1>
+        <p className="pt-5 text-base font-normal text-tcolor">
+          YOU ACKNOWLEDGE THAT YOU HAVE READ THIS AGREEMENT AND AGREE TO ALL ITS
+          TERMS AND CONDITIONS. YOU UNDERSTAND THAT WE MAY AT ANY TIME (DIRECTLY
+          OR INDIRECTLY) SOLICIT CUSTOMER REFERRALS ON TERMS THAT MAY DIFFER
+          FROM THOSE CONTAINED IN THIS AGREEMENT OR OPERATE WEB SITES THAT ARE
+          SIMILAR TO OR COMPETE WITH YOUR WEB SITE. YOU HAVE INDEPENDENTLY
+          EVALUATED THE DESIRABILITY OF PARTICIPATING IN THE PROGRAM AND ARE NOT
+          RELYING ON ANY REPRESENTATION, GUARANTEE, OR STATEMENT OTHER THAN AS
+          SET FORTH IN THIS AGREEMENT.
+        </p>
+      </div>
+      <div className="my-[80px] lg:mb-[120px]">
+        <YellowCard />
       </div>
     </div>
   );
