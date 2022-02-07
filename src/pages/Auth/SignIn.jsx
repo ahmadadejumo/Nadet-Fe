70c1b5eab73f961908914b51677fa127816187c6
ahmadadejumo <adejumoahmad@gmail.com>
2022-02-07 16:46:03 +0100

    working on signup page

--- src/pages/Auth/SignIn.jsx
+++ src/pages/Auth/SignIn.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import Navbar from "../../components/Navbar";
 
 const SignIn = () => {
-  return <div className=""></div>;
+  return (
+    <div className="font-Lato bg-[#F5F4FD]">
+      <div>
+        <Navbar />
+      </div>
+    </div>
+  );
 };
 
 export default SignIn;
