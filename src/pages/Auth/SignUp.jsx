58a590efac16b3b6da9914a8ffe73ebdae940880
ahmadadejumo <adejumoahmad@gmail.com>
2022-02-08 08:55:37 +0100

    adding validation in signup page

--- src/pages/Auth/SignUp.jsx
+++ src/pages/Auth/SignUp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Navbar from "../../components/Navbar";
 import { EyeOffIcon } from "@heroicons/react/outline";
 import { EyeIcon } from "@heroicons/react/outline";
@@ -8,8 +8,75 @@ import twitter from "../../assets/images/twitter.png";
 import rectangle32 from "../../assets/images/rectangle32.png";
 import { Link } from "react-router-dom";
 
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const EMAIL_REGEX =
+  /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+const FULL_REGEX = /^[a-zA-Z]{4,}(?: [a-zA-Z]+){0,2}$/;
+
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
+
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [email, setEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [EmailFocus, setEmailFocus] = useState(false);
+
+  const [fullName, setFullName] = useState("");
+  const [validFullName, setValidFullName] = useState(false);
+  const [fullNameFocus, setFullNameFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const userRef = useRef();
+  const emailRef = useRef();
+  const fullNameRef = useRef();
+  const errRef = useRef();
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    fullNameRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setValidName(USER_REGEX.test(user));
+  }, [user]);
+
+  useEffect(() => {
+    setValidEmail(EMAIL_REGEX.test(email));
+  }, [email]);
+
+  useEffect(() => {
+    setValidFullName(FULL_REGEX.test(fullName));
+  }, [fullName]);
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [user, pwd, matchPwd, email, fullName]);
 
   const handleClick = () => {
     return setShowPassword(showPassword ? false : true);
@@ -19,6 +86,13 @@ const SignUp = () => {
       <Navbar />
       <div className="md:flex md:justify-center lg:mx-[40px] lg:space-x-[30px] md:pt-10 md:pb-14">
         <div className="px-5 md:px-10 md:bg-white md:w-[610px] md:rounded-[40px] lg:rounded-tl-[40px] lg:rounded-bl-[40px] lg:rounded-tr-none lg:rounded-br-none">
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
           <h1 className="text-center text-[25px] font-bold pt-[100px]">
             Create Your Free Account
           </h1>
@@ -28,14 +102,22 @@ const SignUp = () => {
               Login.
             </Link>
           </p>
-          <form action="" className="pt-10 space-y-5">
+          <form className="pt-10 space-y-5">
             <div className="flex flex-col">
-              <label htmlFor="text" className="text-lg font-normal">
+              <label htmlFor="username" className="text-lg font-normal">
                 Username
               </label>
               <input
                 type="text"
                 required
+                id="username"
+                ref={userRef}
+                autoComplete="off"
+                onChange={(e) => setUser(e.target.value)}
+                aria-invalid={validName ? "false" : "true"}
+                aria-describedby="uidnote"
+                onFocus={() => setUserFocus(true)}
+                onBlur={() => setUserFocus(false)}
                 className="h-[50px] w-full rounded-lg outline-none border-[1px] px-5 text-lg"
               />
             </div>
