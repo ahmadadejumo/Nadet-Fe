0343b86b3ec317e14a6a96261ab7e876e7efb762
ahmadadejumo <adejumoahmad@gmail.com>
2022-02-11 14:17:15 +0100

    add a persist login page

---
+++ src/components/PersistLogin.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth, persist } = useAuth();
+
+  useEffect(() => {
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    !auth?.access_token ? verifyRefreshToken() : setIsLoading(false);
+  }, []);
+  return <div></div>;
+};
+
+export default PersistLogin;
