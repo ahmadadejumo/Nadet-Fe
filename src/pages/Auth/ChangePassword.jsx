446e2d5a809fcf689c2975784fd539ce986681ca
ahmadadejumo <adejumoahmad@gmail.com>
2022-02-21 13:07:20 +0100

    add password validation

--- src/pages/Auth/ChangePassword.jsx
+++ src/pages/Auth/ChangePassword.jsx
@@ -1,17 +1,71 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Navbar from "../../components/Navbar";
 import { EyeOffIcon } from "@heroicons/react/outline";
 import { EyeIcon } from "@heroicons/react/outline";
 import { Link, useParams } from "react-router-dom";
 import rectangle35 from "../../assets/images/rectangle35.png";
+import axios from "../../Api/axios";
+
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 
 const ChangePassword = () => {
   const [showPassword, setShowPassword] = useState(false);
   const { uid, token } = useParams();
 
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+
+  const errRef = useRef();
+
+  useEffect(() => {
+    setValidPwd(PWD_REGEX.test(pwd));
+    setValidMatch(pwd === matchPwd);
+  }, [pwd, matchPwd]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [pwd, matchPwd]);
+
   const handleClick = () => {
     return setShowPassword(showPassword ? false : true);
   };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // if button enabled with JS hack
+    const v1 = PWD_REGEX.test(pwd);
+    if (!v1) {
+      setErrMsg("Invalid Password");
+      return;
+    }
+    try {
+      await axios.post(
+        "/auth/password/reset/confirm/",
+        JSON.stringify({
+          new_password1: pwd,
+          new_password2: matchPwd,
+          uid: uid,
+          token: token,
+        })
+      );
+      // Clear input strings
+      setPwd("");
+      setMatchPwd("");
+    } catch (err) {
+      if (!err?.response) {
+        setErrMsg("No Server Response");
+      }
+      errRef.current.focus();
+    }
+  };
+
   return (
     <div className="font-Lato bg-[#F5F4FD]">
       <Navbar />
@@ -23,13 +77,20 @@ const ChangePassword = () => {
           <p className="font-normal text-base text-tcolor pt-[15px] text-center">
             Reset your password with the new one of your choice below.
           </p>
-          <form action="" className="pt-10 space-y-[30px]">
+          <form onSubmit={handleSubmit} className="pt-10 space-y-[30px]">
             <div className="flex flex-col">
               <label htmlFor="password" className="text-lg font-normal">
                 New Password
               </label>
               <div className="flex items-center relative">
                 <input
+                  id="password"
+                  onChange={(e) => setPwd(e.target.value)}
+                  value={pwd}
+                  aria-invalid={validPwd ? "false" : "true"}
+                  aria-describedby="confirmnote"
+                  onFocus={() => setPwdFocus(true)}
+                  onBlur={() => setPwdFocus(false)}
                   type={showPassword ? "text" : "password"}
                   required
                   className="outline-none h-[50px] w-full px-5 rounded-lg border-[1px] text-lg "
@@ -50,16 +111,51 @@ const ChangePassword = () => {
                   </div>
                 )}
               </div>
+              <div
+                className={`${
+                  pwdFocus && !validPwd ? "block" : "hidden"
+                } rounded-lg mt-2 bg-hcolor`}
+              >
+                <p className=" px-3 py-3 text-tcolor font-normal text-sm">
+                  8 to 24 characters.
+                  <br />
+                  Must include uppercase and lowercase letters, a number and a
+                  special character.
+                  <br />
+                  Allowed special characters:{" "}
+                  <span aria-label="exclamation mark">!</span>{" "}
+                  <span aria-label="at symbol">@</span>{" "}
+                  <span aria-label="hashtag">#</span>{" "}
+                  <span aria-label="dollar sign">$</span>{" "}
+                  <span aria-label="percent">%</span>
+                </p>
+              </div>
             </div>
             <div className="flex flex-col">
-              <label htmlFor="password" className="text-lg font-normal">
+              <label htmlFor="confirm password" className="text-lg font-normal">
                 Confirm Password
               </label>
               <input
-                type="password"
+                id="confirm password"
+                onChange={(e) => setMatchPwd(e.target.value)}
+                value={matchPwd}
                 required
+                aria-invalid={validMatch ? "false" : "true"}
+                aria-describedby="confirmnote"
+                onFocus={() => setMatchFocus(true)}
+                onBlur={() => setMatchFocus(false)}
+                type="password"
                 className="h-[50px] w-full rounded-lg outline-none border-[1px] px-5 text-lg"
               />
+            </div>
+            <div
+              className={`${
+                matchFocus && !validMatch ? "block" : "hidden"
+              } rounded-lg mt-2 bg-hcolor`}
+            >
+              <p className=" px-3 py-3 text-tcolor font-normal text-sm">
+                Must match the first password input field.
+              </p>
             </div>
             <div className="flex justify-center md:pt-[11px]">
               <button className="bg-bcolor h-[50px] w-[335px] md:w-full rounded-lg text-base font-bold">
