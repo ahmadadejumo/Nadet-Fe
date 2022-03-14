0b32eee62986d6955b7c8b47df9b663f9025054c
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-14 08:57:16 +0100

    add focus visible library to remove the outline in button

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
+import "focus-visible/dist/focus-visible";
 
 ReactDOM.render(
   <ChakraProvider>
