d22a739fc2e543db22de7ef70d6d07ece7f5dc8c
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-18 18:20:19 +0100

    add image and input for search

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import X from "../assets/images/X.svg";
+import search from "../assets/images/search.svg";
 
 const SearchPage = () => {
   return (
-    <div className="px-5">
+    <div className="px-5 font-Lato">
       <div className="flex justify-end pr-[6px] pt-[32px]">
         <img src={X} alt="XIcon" className="object-contain w-[24px]" />
+      </div>
+      <div>
+        <h1 className="text-center font-bold text-[25px]">Search</h1>
+        <p className="text-center text-base font-normal">
+          What do you expect to find today?
+        </p>
+        <div className="flex justify-center">
+          <div className="flex justify-between px-5 mt-[24px] border shadow-sm shadow-[#22222214] items-center w-[335px] h-[45px] bg-white rounded-[10px]">
+            <input
+              type="text"
+              placeholder="Search for a product"
+              className="border-none outline-none"
+            />
+            <img src={search} alt="search icon" className="w-5" />
+          </div>
+        </div>
+        <h1 className="text-lg font-bold pt-[60px]">Top Searches</h1>
       </div>
     </div>
   );
