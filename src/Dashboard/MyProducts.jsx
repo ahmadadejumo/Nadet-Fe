457495031c508534026ad2059a545b9d9283a83d
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-25 11:05:32 +0100

    add image and constainer

--- src/Dashboard/MyProducts.jsx
+++ src/Dashboard/MyProducts.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Add from "../assets/images/Add.svg";
 import filter from "../assets/images/filter.svg";
+import exportIcon from "../assets/images/export.svg";
 import DropdownSelect from "../components/DropdownSelect";
 
 const MyProducts = () => {
@@ -13,12 +14,22 @@ const MyProducts = () => {
           ADD PRODUCTS
         </button>
       </div>
-      <div>
+      <div className="flex justify-around pt-10">
         <DropdownSelect name="All Type" width={"w-[94px]"} />
         <DropdownSelect name="All Category" width={"w-[130px]"} />
         <div className="bg-white rounded-md w-[35px] h-[30px] flex justify-center items-center">
           <img src={filter} alt="filetrIcon" className="w-[12px] h-[14.65px]" />
         </div>
+      </div>
+      <div className="bg-white rounded-lg w-[213px] h-[32px] flex justify-between px-5 items-center mt-[26.48px] mx-3">
+        <h1 className="font-bold text-base text-[#35363AB2]">
+          Export Data in CSV
+        </h1>
+        <img
+          src={exportIcon}
+          alt="exportIcon"
+          className="w-[14.17px] h-[16.67px]"
+        />
       </div>
     </div>
   );
