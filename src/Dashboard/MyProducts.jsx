95b7c40fdfff638e5ff8c9b22438f1a62ddc953f
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-25 10:22:40 +0100

    add text and button

--- src/Dashboard/MyProducts.jsx
+++ src/Dashboard/MyProducts.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Add from "../assets/images/Add.svg";
 
 const MyProducts = () => {
-  return <div>MyProducts</div>;
+  return (
+    <div className="font-Lato pt-[44px]">
+      <h1 className="text-center font-bold text-[25px]">All Products</h1>
+      <div className="flex justify-center pt-[16px]">
+        <button className="flex justify-between text-base font-semibold items-center bg-bcolor w-[205px] h-[50px] rounded-lg px-7">
+          <img src={Add} alt="addIcon" />
+          ADD PRODUCTS
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default MyProducts;
