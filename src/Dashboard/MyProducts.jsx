d49761602d7f01bb80b470cd62fa6ac550cf6681
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-26 11:35:14 +0100

    add media queries for desktop responsiveness

--- src/Dashboard/MyProducts.jsx
+++ src/Dashboard/MyProducts.jsx
@@ -7,41 +7,46 @@ import ProductTable from "../components/ProductTable";
 
 const MyProducts = () => {
   return (
-    <div className="font-Lato pt-[44px] h-screen">
-      <h1 className="text-center font-bold text-[25px] md:text-3xl">
-        All Products
-      </h1>
-      <div className="flex justify-center pt-[16px] md:pt-8">
-        <button className="flex justify-between text-base font-semibold items-center bg-bcolor w-[205px] h-[50px] rounded-lg px-7">
-          <img src={Add} alt="addIcon" />
-          ADD PRODUCTS
-        </button>
-      </div>
-      <div className="md:flex md:justify-between px-10 items-center pt-10 md:pt-12">
-        <div className="flex justify-around md:justify-center md:items-center md:space-x-5">
-          <DropdownSelect name="All Type" width={"w-[94px]"} />
-          <DropdownSelect name="All Category" width={"w-[130px]"} />
-          <div className="bg-white rounded-md w-[35px] h-[30px] flex justify-center items-center">
+    <div className="flex justify-center">
+      <div className="font-Lato pt-[44px] h-screen lg:px-[115px]">
+        <div className="lg:flex lg:justify-between lg:items-center lg:pt-[55px]">
+          <h1 className="text-center font-bold text-[25px] md:text-3xl lg:text-[28px]">
+            All Products
+          </h1>
+          <div className="flex justify-center pt-[16px] md:pt-8 lg:pt-0">
+            <button className="flex justify-between text-base font-semibold items-center bg-bcolor w-[205px] h-[50px] rounded-lg px-7">
+              <img src={Add} alt="addIcon" />
+              ADD PRODUCTS
+            </button>
+          </div>
+        </div>
+
+        <div className="md:flex md:justify-between md:px-10 lg:px-0 md:items-center md:pt-12 lg:pt-[30px]">
+          <div className="flex justify-around md:justify-center md:items-center md:space-x-5 pt-10 md:pt-0">
+            <DropdownSelect name="All Type" width={"w-[94px]"} />
+            <DropdownSelect name="All Category" width={"w-[130px]"} />
+            <div className="bg-white rounded-md w-[35px] h-[30px] flex justify-center items-center">
+              <img
+                src={filter}
+                alt="filetrIcon"
+                className="w-[12px] h-[14.65px]"
+              />
+            </div>
+          </div>
+          <div className="bg-white rounded-lg w-[213px] h-[32px] flex justify-between px-5 items-center mt-[26.48px] md:mt-0 mx-3 lg:mx-0">
+            <h1 className="font-bold text-base text-[#35363AB2]">
+              Export Data in CSV
+            </h1>
             <img
-              src={filter}
-              alt="filetrIcon"
-              className="w-[12px] h-[14.65px]"
+              src={exportIcon}
+              alt="exportIcon"
+              className="w-[14.17px] h-[16.67px]"
             />
           </div>
         </div>
-        <div className="bg-white rounded-lg w-[213px] h-[32px] flex justify-between px-5 items-center mt-[26.48px] md:mt-0 mx-3">
-          <h1 className="font-bold text-base text-[#35363AB2]">
-            Export Data in CSV
-          </h1>
-          <img
-            src={exportIcon}
-            alt="exportIcon"
-            className="w-[14.17px] h-[16.67px]"
-          />
+        <div className="flex justify-center">
+          <ProductTable />
         </div>
-      </div>
-      <div className="flex justify-center">
-        <ProductTable />
       </div>
     </div>
   );
