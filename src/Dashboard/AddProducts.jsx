20e8d005977c669b3308bdd3b2593fe8e68266da
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-28 12:30:43 +0100

    add tablet responsiveness to styling

--- src/Dashboard/AddProducts.jsx
+++ src/Dashboard/AddProducts.jsx
@@ -3,10 +3,12 @@ import ProductContainer from "../components/ProductContainer";
 
 const AddProducts = () => {
   return (
-    <div className="font-Lato px-5 h-screen">
-      <h1 className="font-bold text-xl pt-10">Add Product</h1>
+    <div className="font-Lato px-5 md:px-14 h-screen">
+      <h1 className="font-bold text-xl md:text-2xl pt-10 md:pt-14">
+        Add Product
+      </h1>
       <hr className="lg:hidden mt-[16px]" />
-      <div className="pt-[24px] grid grid-cols-2 gap-5 pb-[46px]">
+      <div className="pt-[24px] md:pt-10 grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 pb-[46px]">
         <ProductContainer />
         <ProductContainer />
         <ProductContainer />
