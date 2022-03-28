789fd8ba3194d72bc63bed8b3775d062283393d7
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-28 13:17:35 +0100

    map types of product data to the reusable component

--- src/Dashboard/AddProducts.jsx
+++ src/Dashboard/AddProducts.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ProductContainer from "../components/ProductContainer";
+import { typesOfProductData } from "../components/TypesOfProductData";
 
 const AddProducts = () => {
   return (
@@ -9,12 +10,9 @@ const AddProducts = () => {
       </h1>
       <hr className="lg:hidden mt-[16px]" />
       <div className="pt-[24px] md:pt-10 grid grid-cols-2 md:grid-cols-3 gap-5 md:gap-10 lg:gap-x-[64px] gap-y-[77px] pb-[46px]">
-        <ProductContainer />
-        <ProductContainer />
-        <ProductContainer />
-        <ProductContainer />
-        <ProductContainer />
-        <ProductContainer />
+        {typesOfProductData.map(({ icon, name, desc }, index) => (
+          <ProductContainer key={index} icon={icon} desc={desc} name={name} />
+        ))}
       </div>
     </div>
   );
