b37e5370f0d6b0883fb7d690e5713844e05c242f
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-29 14:15:42 +0100

    add image

--- src/Dashboard/DigitalProducts.jsx
+++ src/Dashboard/DigitalProducts.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import leftarrow from "../assets/images/leftarrow.svg";
 
 const DigitalProducts = () => {
-  return <div>DigitalProducts</div>;
+  return (
+    <div>
+      <div>
+        <img src={leftarrow} alt="arrow" />
+        <p>Back</p>
+      </div>
+    </div>
+  );
 };
 
 export default DigitalProducts;
