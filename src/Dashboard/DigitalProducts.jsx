db08045ea26cb6e3baa829bb4329ed292a2e1b68
ahmadadejumo <adejumoahmad@gmail.com>
2022-03-31 10:43:36 +0100

    add product details component

--- src/Dashboard/DigitalProducts.jsx
+++ src/Dashboard/DigitalProducts.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import leftarrow from "../assets/images/leftarrow.svg";
+import ProductDetails from "../components/ProductDetails";
 
 const DigitalProducts = () => {
   return (
-    <div>
-      <div>
+    <div className="font-Lato px-5">
+      <div className="flex space-x-4 items-center pt-[24px]">
         <img src={leftarrow} alt="arrow" />
-        <p>Back</p>
+        <p className="text-lg font-normal">Back</p>
+      </div>
+      <h1 className="font-bold text-xl pt-[32px]">Add Product</h1>
+      <div>
+        <ProductDetails />
       </div>
     </div>
   );
---
+++ src/components/ProductDetails.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductDetails = () => {
+  return (
+    <div className="bg-white font-Lato px-[24px]">
+      <h1 className="pt-[24px] font-bold text-[20px]">Product details</h1>
+      <p className="text-sm">Basic product details</p>
+      <h1 className="text-xl font-medium pt-[32px]">
+        Images<span className="text-[#FBBC15]">*</span>
+      </h1>
+    </div>
+  );
+};
+
+export default ProductDetails;
