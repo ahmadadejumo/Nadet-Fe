a464bcab989c0c26f9ddb1d2483a70f37087c0d1
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-04 23:21:44 +0100

    add badge to indicate new feature in tabs

--- src/Dashboard/DigitalProducts.jsx
+++ src/Dashboard/DigitalProducts.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import leftarrow from "../assets/images/leftarrow.svg";
 import ProductDetails from "../components/ProductDetails";
 import { useNavigate } from "react-router-dom";
 
 const DigitalProducts = () => {
   const navigate = useNavigate();
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
   return (
     <div className="font-Lato lg:px-[150px]">
@@ -18,6 +23,67 @@ const DigitalProducts = () => {
       <h1 className="font-bold text-xl pt-[32px] px-5">Add Product</h1>
       <div>
         <ProductDetails />
+      </div>
+      {/* Products information */}
+      <div className="mt-[25px] bg-white">
+        <div className="flex justify-between pt-[25px] items-center px-[30px]">
+          <p
+            onClick={() => toggleTab(1)}
+            className={`${
+              toggleState === 1 ? "bg-bcolor" : "bg-[#C4C4C4]"
+            } rounded-full text-xs font-bold w-[28px] h-[28px] flex justify-center items-center`}
+          >
+            1
+          </p>
+          <hr className="border-black border-dashed w-[36%]" />
+          <p
+            onClick={() => toggleTab(2)}
+            className={`${
+              toggleState === 2 ? "bg-bcolor" : "bg-[#C4C4C4]"
+            } rounded-full text-xs font-bold w-[28px] h-[28px] flex justify-center items-center`}
+          >
+            2
+          </p>
+          <hr className="border-black border-dashed w-[36%]" />
+          <p
+            onClick={() => toggleTab(3)}
+            className={`${
+              toggleState === 3 ? "bg-bcolor" : "bg-[#C4C4C4]"
+            } rounded-full text-xs font-bold w-[28px] h-[28px] flex justify-center items-center`}
+          >
+            3
+          </p>
+        </div>
+        <div className="flex justify-between text-xs font-bold items-center px-[24px] pt-[12px]">
+          <p
+            className={`${
+              toggleState === 1 ? "text-black" : "text-[#575555]"
+            } text-center`}
+          >
+            More
+            <br /> details
+          </p>
+          <div className="flex items-center space-x-2">
+            <p
+              className={`${
+                toggleState === 2 ? "text-black" : "text-[#575555]"
+              } text-center`}
+            >
+              Upsells &<br /> Cross-sells
+            </p>
+            <div className="bg-[#FA497C] rounded w-[43px] h-[22px] flex items-center justify-center text-white">
+              New
+            </div>
+          </div>
+
+          <p
+            className={`${
+              toggleState === 3 ? "text-black" : "text-[#575555]"
+            } text-center`}
+          >
+            Advanced <br /> Options
+          </p>
+        </div>
       </div>
     </div>
   );
