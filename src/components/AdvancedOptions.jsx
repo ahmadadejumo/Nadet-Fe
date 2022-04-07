e9fd3873144d7694c2bbb175bd6285768414889d
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-07 11:39:54 +0100

    add checkbox button

--- src/components/AdvancedOptions.jsx
+++ src/components/AdvancedOptions.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AdvancedOptions = () => {
+  const [hideProduct, setHideProduct] = useState(false);
+
+  const handleHideproduct = () => {
+    setHideProduct(!hideProduct);
+  };
+
   return (
     <div className="px-[24px] md:px-[35px] pt-[24px] md:pt-[47px] font-Lato">
       <h1 className="text-base font-bold md:text-[20px]">
@@ -14,6 +20,18 @@ const AdvancedOptions = () => {
       <p className="text-xs font-normal pt-1">
         Override the default label for the checkout button on the product page.
       </p>
+      <div className="flex items-center space-x-[12px] mt-[24px]">
+        <input
+          type="checkbox"
+          //   defaultChecked={!hideProduct}
+          onClick={handleHideproduct}
+          className="form-checkbox text-bcolor cursor-pointer w-[17px] h-[17px] border rounded"
+        />
+        <p className="text-[15px] font-medium">
+          Hide this product from my store homepage (Only accessible via the
+          direct product link)
+        </p>
+      </div>
     </div>
   );
 };
