c1aed0615125c0ecdce8d30b08051fa7e981c756
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-07 12:02:22 +0100

    add some stylings and backward navigation

--- src/Dashboard/Tickets.jsx
+++ src/Dashboard/Tickets.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import leftarrow from "../assets/images/leftarrow.svg";
+import ProductDetails from "../components/ProductDetails";
+import { useNavigate } from "react-router-dom";
 
 const Tickets = () => {
-  return <div>Tickets</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="font-Lato lg:px-[150px]">
+      <div
+        onClick={() => navigate(-1)}
+        className="flex space-x-4 items-center pt-[24px] px-5 cursor-pointer"
+      >
+        <img src={leftarrow} alt="arrow" />
+        <p className="text-lg font-normal">Back</p>
+      </div>
+      <h1 className="font-bold text-xl pt-[32px] px-5">Add Product</h1>
+      <div>
+        <ProductDetails />
+      </div>
+      {/* Products information */}
+      <div></div>
+    </div>
+  );
 };
 
 export default Tickets;
