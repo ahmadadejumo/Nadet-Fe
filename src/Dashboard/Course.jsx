d3315b36d607eaac7e1971d7ff6f4126ac5a20a4
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-08 09:18:25 +0100

    add reujsable components to new page

--- src/Dashboard/Course.jsx
+++ src/Dashboard/Course.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import leftarrow from "../assets/images/leftarrow.svg";
+import ProductDetails from "../components/ProductDetails";
+import { useNavigate } from "react-router-dom";
+import ProductTab from "../components/ProductTab";
+import UpAndCrossSells from "../components/UpAndCrossSells";
+import AdvancedOptions from "../components/AdvancedOptions";
+import { Select } from "@chakra-ui/react";
 
 const Course = () => {
-  return <div>Course</div>;
+  const navigate = useNavigate();
+  const [toggleState, setToggleState] = useState(1);
+  const [redirectUrl, setRedirectUrl] = useState(false);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
+  const showRedirectUrl = () => {
+    setRedirectUrl(!redirectUrl);
+  };
+
+  return (
+    <div className="font-Lato lg:px-[150px]">
+      <div
+        onClick={() => navigate(-1)}
+        className="flex space-x-4 items-center pt-[24px] px-5 cursor-pointer"
+      >
+        <img src={leftarrow} alt="arrow" />
+        <p className="text-lg font-normal">Back</p>
+      </div>
+      <h1 className="font-bold text-xl pt-[32px] px-5">Add Product</h1>
+      <div>
+        <ProductDetails />
+      </div>
+      {/* Products information */}
+      <div className="mt-[25px] bg-white mb-[80px] lg:mb-[157px]">
+        {/* Product Detail Tab component */}
+        <ProductTab toggleTab={toggleTab} toggleState={toggleState} />
+      </div>
+    </div>
+  );
 };
 
 export default Course;
