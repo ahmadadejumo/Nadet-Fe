c3acb7a1bb334d85f95e3140dd87be5447f90949
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-11 22:58:52 +0100

    handle states

--- src/Dashboard/Subscription.jsx
+++ src/Dashboard/Subscription.jsx
@@ -6,6 +6,7 @@ import ProductTab from "../components/ProductTab";
 import UpAndCrossSells from "../components/UpAndCrossSells";
 import AdvancedOptions from "../components/AdvancedOptions";
 import { Select } from "@chakra-ui/react";
+import SubscriptionContainer from "../components/SubscriptionContainer";
 
 const Subscription = () => {
   const navigate = useNavigate();
@@ -13,9 +14,9 @@ const Subscription = () => {
   const [intervals, setIntervals] = useState("");
   const [amount, setAmount] = useState(0);
   const [timesCharged, setTimesCharged] = useState(0);
+  const [sum] = useState(1);
   const [dropdown, setDropdown] = useState(false);
-  const [container, setContainer] = useState([]);
-  const [addValue, setAddValue] = useState(0);
+  const [containerData, setContainerData] = useState([]);
 
   const toggleTab = (index) => {
     setToggleState(index);
@@ -25,10 +26,24 @@ const Subscription = () => {
     setDropdown(!dropdown);
   };
 
+  const n = parseFloat(sum) + parseFloat(timesCharged);
+
   const handleSubscriptionTier = (e) => {
     e.preventDefault();
-    setContainer(!container);
-    setAddValue(timesCharged + 1);
+    setAmount("");
+    setIntervals("");
+    setTimesCharged("");
+    setContainerData([
+      ...containerData,
+      { amount: amount, intervals: intervals, timesCharged: timesCharged },
+    ]);
+    console.log(containerData);
+    return;
+  };
+
+  const removeItem = (index) => {
+    containerData.splice(index, 1);
+    setContainerData(...containerData);
   };
 
   return (
@@ -55,81 +70,75 @@ const Subscription = () => {
               <p>Subscription</p>
             </div>
             <hr className="bg-[#E8E8EB99] mt-[24px]" />
-            <div className="border px-5 rounded-lg mt-5">
-              <h1 className="text-[20px] font-medium pt-[24px]">
-                Subscription Intervals (Tiers):
-              </h1>
-              <p className="pt-[16px] text-sm font-normal">
-                Create the subscription interval options your customer can
-                subscribe to
-              </p>
-              <div className="pt-[12px]">
-                <Select
-                  onChange={(e) => setIntervals(e.target.value)}
-                  placeholder="Select Interval"
-                  size="lg"
-                >
-                  <option value="Weekly">Every week</option>
-                  <option value="Monthly">Every month</option>
-                  <option value="Quarterly">Every quarter</option>
-                  <option value="Bi-annual">Bi-annually(6 months)</option>
-                  <option value="Custom">Custom Interval</option>
-                </Select>
-              </div>
-              <h1 className="text-xl font-medium pt-[16px]">
-                Amount (NGN)<span className="text-[#FBBC15]">*</span>
-              </h1>
-              <input
-                type="number"
-                onChange={(e) => setAmount(e.target.value)}
-                value={amount}
-                placeholder="500"
-                className="h-[44px] w-[169px] pl-[16px] border rounded border-[#E8E8EB] mt-1 outline-none text-base"
-              />
-              <h1 className="text-sm font-medium pt-[16px]">
-                No of times the customer will be charged after their first
-                payment
-              </h1>
-              <input
-                type="number"
-                onChange={(e) => setTimesCharged(e.target.value)}
-                placeholder="0"
-                className="h-[44px] w-full pl-[16px] border rounded border-[#E8E8EB] mt-1 outline-none text-base"
-              />
-              <p className="text-xs pb-5">
-                Set <span className="text-bcolor">0</span> if you want the
-                customer to be charge continuously until they decide to cancel.
-              </p>
-            </div>
-            {container && (
-              <div className="border rounded-lg mt-5">
-                <div className="flex justify-between items-center px-3">
-                  <h1
-                    onClick={showDropdown}
-                    className="py-3 text-base font-semibold cursor-pointer"
+            <form onSubmit={handleSubscriptionTier}>
+              <div className="border px-5 rounded-lg mt-5">
+                <h1 className="text-[20px] font-medium pt-[24px]">
+                  Subscription Intervals (Tiers):
+                </h1>
+                <p className="pt-[16px] text-sm font-normal">
+                  Create the subscription interval options your customer can
+                  subscribe to
+                </p>
+                <div className="pt-[12px]">
+                  <Select
+                    onChange={(e) => setIntervals(e.target.value)}
+                    placeholder="Select intervals"
+                    size="lg"
                   >
-                    {intervals} Subscription
-                  </h1>
-                  <p className="text-xl font-bold text-red-500 cursor-pointer">
-                    X
-                  </p>
+                    <option value="Weekly">Every week</option>
+                    <option value="Monthly">Every month</option>
+                    <option value="Quarterly">Every quarter</option>
+                    <option value="Bi-annual">Bi-annually (6 months)</option>
+                    <option value="Yearly">Every year</option>
+                  </Select>
                 </div>
-                {dropdown && (
-                  <div className="border-t py-2 px-2 text-sm font-medium">
-                    This is a recurring payment subscription of {addValue}{" "}
-                    payments. After the first payment, the customer will be
-                    charged {timesCharged} more times (every {intervals}).
-                  </div>
-                )}
+                <h1 className="text-xl font-medium pt-[16px]">
+                  Amount (NGN)<span className="text-[#FBBC15]">*</span>
+                </h1>
+                <input
+                  type="number"
+                  onChange={(e) => setAmount(e.target.value)}
+                  placeholder="500"
+                  className="h-[44px] w-[169px] pl-[16px] border rounded border-[#E8E8EB] mt-1 outline-none text-base"
+                />
+                <h1 className="text-sm font-medium pt-[16px]">
+                  No of times the customer will be charged after their first
+                  payment
+                </h1>
+                <input
+                  type="number"
+                  onChange={(e) => setTimesCharged(e.target.value)}
+                  placeholder="0"
+                  className="h-[44px] w-full pl-[16px] border rounded border-[#E8E8EB] mt-1 outline-none text-base"
+                />
+                <p className="text-xs pb-5">
+                  Set <span className="text-bcolor">0</span> if you want the
+                  customer to be charge continuously until they decide to
+                  cancel.
+                </p>
               </div>
-            )}
+              {containerData.map(({ timesCharged, intervals }, index) => {
+                return (
+                  <SubscriptionContainer
+                    key={index}
+                    timesCharged={timesCharged}
+                    intervals={intervals}
+                    n={n}
+                    showDropdown={showDropdown}
+                    dropdown={dropdown}
+                    removeItem={removeItem}
+                  />
+                );
+              })}
 
-            <button
-              onClick={() => handleSubscriptionTier()}
-              className="bg-gray-500 w-[200px] h-[42px] text-white rounded mt-[16px] text-base font-bold"
-            >
-              Add Subscription Tier
-            </button>
+              <button
+                disabled={!intervals || !amount}
+                type="submit"
+                className="bg-gray-500 w-[200px] h-[42px] text-white rounded mt-[16px] text-base font-bold"
+              >
+                Add Subscription Tier
+              </button>
+            </form>
           </div>
         )}
         {toggleState === 2 && <UpAndCrossSells />}
---
+++ src/components/SubscriptionContainer.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+const SubscriptionContainer = ({
+  timesCharged,
+  intervals,
+  n,
+  dropdown,
+  showDropdown,
+  removeItem,
+}) => {
+  return (
+    <div className="border rounded-lg mt-5">
+      <div className="flex justify-between items-center px-3">
+        <h1
+          onClick={showDropdown}
+          className="py-3 text-base font-semibold cursor-pointer"
+        >
+          {intervals} Subscription
+        </h1>
+        <p
+          onClick={() => removeItem}
+          className="text-xl font-bold text-red-500 cursor-pointer"
+        >
+          X
+        </p>
+      </div>
+      {dropdown && (
+        <div className="border-t py-2 px-2 text-sm font-medium">
+          {timesCharged === 0 ? (
+            <p>
+              The customer will be charged every{" "}
+              {intervals === "Weekly"
+                ? "week"
+                : intervals === "Monthly"
+                ? "month"
+                : intervals === "Quarterly"
+                ? "quarter"
+                : intervals === "Bi-annual"
+                ? "6 months"
+                : "year"}
+              .
+            </p>
+          ) : (
+            <p>
+              This is a recurring payment subscription of {n} payments. After
+              the first payment, the customer will be charged {timesCharged}{" "}
+              more times (every{" "}
+              {intervals === "Weekly"
+                ? "week"
+                : intervals === "Monthly"
+                ? "month"
+                : intervals === "Quarterly"
+                ? "quarter"
+                : intervals === "Bi-annual"
+                ? "6 months"
+                : "year"}
+              ).
+            </p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SubscriptionContainer;
