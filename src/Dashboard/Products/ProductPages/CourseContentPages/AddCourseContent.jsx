deba0a905a1e2e5a570ec8ccaf678d9bd18048a5
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-13 23:14:21 +0100

    add text, styling and buttons

--- src/Dashboard/Products/ProductPages/CourseContentPages/AddCourseContent.jsx
+++ src/Dashboard/Products/ProductPages/CourseContentPages/AddCourseContent.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import BackNavigation from "../../../../components/BackNavigation";
 
 const AddCourseContent = () => {
-  return <div>AddCourseContent</div>;
+  return (
+    <div className="font-Lato">
+      <BackNavigation />
+      <div className="pt-[32px] px-5">
+        <h1 className="font-bold text-xl">Course content</h1>
+        <p className="text-[20px] font-medium pt-1">Manage course curriculum</p>
+        <div className="flex justify-between space-x-[25px] pt-[24px]">
+          <button className="bg-bcolor h-[38px] rounded w-full font-bold text-xs">
+            Set Preview Video
+          </button>
+          <button className="bg-bcolor h-[38px] rounded w-full font-bold text-xs">
+            Preview Course
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AddCourseContent;
