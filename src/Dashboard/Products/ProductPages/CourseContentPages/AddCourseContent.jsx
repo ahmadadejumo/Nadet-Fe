5295c25616e39723b8a766e83653f502b2e575a1
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-13 23:28:05 +0100

    add image and containers

--- src/Dashboard/Products/ProductPages/CourseContentPages/AddCourseContent.jsx
+++ src/Dashboard/Products/ProductPages/CourseContentPages/AddCourseContent.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import BackNavigation from "../../../../components/BackNavigation";
+import pen from "../../../../assets/images/pen.svg";
 
 const AddCourseContent = () => {
   return (
@@ -15,6 +16,12 @@ const AddCourseContent = () => {
           <button className="bg-bcolor h-[38px] rounded w-full font-bold text-xs">
             Preview Course
           </button>
+        </div>
+      </div>
+      <div>
+        <div className="bg-[#8492A642] flex">
+          <p className="text-base font-bold">First Section</p>
+          <img src={pen} alt="icon" />
         </div>
       </div>
     </div>
