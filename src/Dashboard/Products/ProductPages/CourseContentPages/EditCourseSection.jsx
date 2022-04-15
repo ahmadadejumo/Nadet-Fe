56051340d7b9b68be3c90cdd1832de02d1881117
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-15 16:44:31 +0100

    add button, text and styling

--- src/Dashboard/Products/ProductPages/CourseContentPages/EditCourseSection.jsx
+++ src/Dashboard/Products/ProductPages/CourseContentPages/EditCourseSection.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import BackNavigation from "../../../../components/BackNavigation";
 
 const EditCourseSection = () => {
-  return <div>EditCourseSection</div>;
+  return (
+    <div className="font-Lato">
+      <div className="px-5">
+        <BackNavigation />
+        <h1 className="pt-[32px] text-[24px] font-bold">Course content</h1>
+        <p className="text-[20px] font-medium pt-1">Lecture 1</p>
+        <button className="w-[208px] h-[37px] text-xs font-bold rounded mt-[24px] bg-bcolor">
+          Upload Course Resources
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default EditCourseSection;
