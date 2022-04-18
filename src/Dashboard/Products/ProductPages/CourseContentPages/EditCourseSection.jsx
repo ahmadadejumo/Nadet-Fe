8577c51008e217f227d36215d3fc710f4270e567
ahmadadejumo <adejumoahmad@gmail.com>
2022-04-18 09:32:59 +0100

    add input and styling

--- src/Dashboard/Products/ProductPages/CourseContentPages/EditCourseSection.jsx
+++ src/Dashboard/Products/ProductPages/CourseContentPages/EditCourseSection.jsx
@@ -81,68 +81,79 @@ const EditCourseSection = () => {
           </button>
         </div>
         <div className="px-5">
-          <Files
-            className="files-dropzone"
-            onChange={onFilesChange}
-            onError={onFilesError}
-            accepts={["image/*", ".pdf", "video/mp4"]}
-            maxFileSize={104857600}
-            minFileSize={0}
-            clickable
-          >
-            <div
-              className={`mt-[12px] cursor-pointer shadow-inner shadow-[#E8E8EB] border pt-[12px] pb-[15px] border-dashed border-[#E8E8EB] w-full rounded flex pl-[18px] space-x-3`}
-            >
-              <img src={download} alt="icon" />
-              <p className="text-sm font-normal opacity-60">
-                Drag or <span className="text-[#FBBC15]">upload</span> your file
-              </p>
-            </div>
-          </Files>
-          {files.length > 0 ? (
-            <div className="bg-gray-300 mb-10 rounded mt-2">
-              <ul>
-                {files.map((file) => (
-                  <div className="flex justify-between" key={file.id}>
-                    <li className="flex space-x-2 h-16">
-                      <div className="w-16 flex justify-center">
-                        {file.preview.type === "image" ? (
-                          <img
-                            className="w-16 object-contain"
-                            src={file.preview.url}
-                            alt="file"
-                          />
-                        ) : (
-                          <div className="w-16 text-2xl  h-16 text-center bg-gray-400 pt-4">
-                            {file.extension}
+          {toggleButtonState === 1 ? (
+            <>
+              <Files
+                className="files-dropzone"
+                onChange={onFilesChange}
+                onError={onFilesError}
+                accepts={["image/*", ".pdf", "video/mp4"]}
+                maxFileSize={104857600}
+                minFileSize={0}
+                clickable
+              >
+                <div className="mt-[32px] cursor-pointer shadow-inner shadow-[#E8E8EB] border pt-[12px] pb-[15px] border-dashed border-[#E8E8EB] w-full rounded flex pl-[18px] space-x-3">
+                  <img src={download} alt="icon" />
+                  <p className="text-sm font-normal opacity-60">
+                    Drag or <span className="text-[#FBBC15]">upload</span> your
+                    file
+                  </p>
+                </div>
+              </Files>
+              {files.length > 0 ? (
+                <div className="bg-gray-300 mb-10 rounded mt-2">
+                  <ul>
+                    {files.map((file) => (
+                      <div className="flex justify-between" key={file.id}>
+                        <li className="flex space-x-2 h-16">
+                          <div className="w-16 flex justify-center">
+                            {file.preview.type === "image" ? (
+                              <img
+                                className="w-16 object-contain"
+                                src={file.preview.url}
+                                alt="file"
+                              />
+                            ) : (
+                              <div className="w-16 text-2xl  h-16 text-center bg-gray-400 pt-4">
+                                {file.extension}
+                              </div>
+                            )}
                           </div>
-                        )}
-                      </div>
-                      <div>
-                        <div className="text-base font-semibold">
-                          {file.name}
+                          <div>
+                            <div className="text-base font-semibold">
+                              {file.name}
+                            </div>
+                            <div className="text-sm font-medium">
+                              {file.sizeReadable}
+                            </div>
+                          </div>
+                        </li>
+                        <div className="flex items-center pr-1 cursor-pointer">
+                          <img
+                            src={X}
+                            alt="icon"
+                            id={file.id}
+                            className="w-5"
+                            onClick={() => filesRemoveOne(file.id)}
+                          />
                         </div>
-                        <div className="text-sm font-medium">
-                          {file.sizeReadable}
-                        </div>
                       </div>
-                    </li>
-                    <div className="flex items-center pr-1 cursor-pointer">
-                      <img
-                        src={X}
-                        alt="icon"
-                        id={file.id}
-                        className="w-5"
-                        onClick={() => filesRemoveOne(file.id)}
-                      />
-                    </div>
-                  </div>
-                ))}
-              </ul>
+                    ))}
+                  </ul>
+                </div>
+              ) : null}
+              {error && (
+                <p className="text-base font-medium text-red-500">{error}</p>
+              )}
+            </>
+          ) : (
+            <div>
+              <input
+                type="text"
+                placeholder="Insert link here..."
+                className="mt-[32px] outline-bcolor shadow-inner shadow-[#E8E8EB] border h-[44px] border-dashed border-[#E8E8EB] w-full rounded pl-[18px]"
+              />
             </div>
-          ) : null}
-          {error && (
-            <p className="text-base font-medium text-red-500">{error}</p>
           )}
         </div>
       </div>
