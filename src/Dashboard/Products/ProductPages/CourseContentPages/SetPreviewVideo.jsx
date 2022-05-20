7872183ecad14766f61ec017613bd7785eb931c9
ahmadadejumo <adejumoahmad@gmail.com>
2022-05-20 17:26:32 +0100

    add a container with styling

--- src/Dashboard/Products/ProductPages/CourseContentPages/SetPreviewVideo.jsx
+++ src/Dashboard/Products/ProductPages/CourseContentPages/SetPreviewVideo.jsx
@@ -1,3 +1,4 @@
+import { ExclamationCircleIcon } from "@heroicons/react/outline";
 import React from "react";
 import BackNavigation from "../../../../components/BackNavigation";
 
@@ -8,6 +9,18 @@ const SetPreviewVideo = () => {
         <BackNavigation />
         <h1 className="font-bold text-2xl pt-[32px]">Course content</h1>
         <p className="font-medium text-xl">Course Settings</p>
+      </div>
+      <div className="flex justify-center mt-[24px] space-x-2 px-5 mx-5 py-[12px] rounded-lg bg-[#FAF1D8] border border-[#555557]">
+        <div>
+          <ExclamationCircleIcon className="h-[25px] w-[25px] text-red-700" />
+        </div>
+
+        <p className="text-xs font-bold text-[#212121CC]">
+          Important Notice: It is advisable to always compress your videos to be
+          as small as possible before uploading them. Creators are recommended
+          to upload videos in the range 20MB to 100MB per video; this is because
+          you want to be considerate to your customers watching the videos.
+        </p>
       </div>
     </div>
   );
