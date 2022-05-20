015d6e817dbc6aee91b717d67b6085a89cb4ff1c
ahmadadejumo <adejumoahmad@gmail.com>
2022-05-20 17:15:26 +0100

    add text and styling

--- src/Dashboard/Products/ProductPages/CourseContentPages/SetPreviewVideo.jsx
+++ src/Dashboard/Products/ProductPages/CourseContentPages/SetPreviewVideo.jsx
@@ -6,6 +6,8 @@ const SetPreviewVideo = () => {
     <div className="font-Lato lg:px-[150px]">
       <div className="px-5 lg:px-0">
         <BackNavigation />
+        <h1 className="font-bold text-2xl pt-[32px]">Course content</h1>
+        <p className="font-medium text-xl">Course Settings</p>
       </div>
     </div>
   );
