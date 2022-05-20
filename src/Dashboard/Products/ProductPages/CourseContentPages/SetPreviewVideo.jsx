dfbdb46eb4524a54418895870d48d9a1a2795f45
ahmadadejumo <adejumoahmad@gmail.com>
2022-05-20 18:07:30 +0100

    add desktop responsiveness

--- src/Dashboard/Products/ProductPages/CourseContentPages/SetPreviewVideo.jsx
+++ src/Dashboard/Products/ProductPages/CourseContentPages/SetPreviewVideo.jsx
@@ -25,25 +25,29 @@ const SetPreviewVideo = () => {
   };
 
   return (
-    <div className="font-Lato lg:px-[150px]">
+    <div className="font-Lato lg:px-[150px] md:h-screen lg:h-0">
       <div className="px-5 md:px-16 lg:px-0">
         <BackNavigation />
-        <h1 className="font-bold text-2xl pt-[32px]">Course content</h1>
-        <p className="font-medium text-xl">Course Settings</p>
+        <h1 className="font-bold text-2xl lg:text-[28px] pt-[32px]">
+          Course content
+        </h1>
+        <p className="font-medium text-xl lg:text-[24px] lg:pt-[16px]">
+          Course Settings
+        </p>
       </div>
-      <div className="flex justify-center mt-[24px] space-x-2 px-5 md:mx-16 mx-5 py-[12px] rounded-lg bg-[#FAF1D8] border border-[#555557]">
+      <div className="flex justify-center mt-[24px] space-x-2 px-5 md:mx-16 lg:mx-0 mx-5 py-[12px] rounded-lg bg-[#FAF1D8] border border-[#555557]">
         <div>
           <ExclamationCircleIcon className="h-[25px] w-[25px] text-red-700" />
         </div>
 
-        <p className="text-xs md:text-sm font-bold text-[#212121CC]">
+        <p className="text-xs md:text-sm lg:text-base font-bold text-[#212121CC]">
           Important Notice: It is advisable to always compress your videos to be
           as small as possible before uploading them. Creators are recommended
           to upload videos in the range 20MB to 100MB per video; this is because
           you want to be considerate to your customers watching the videos.
         </p>
       </div>
-      <div className="mt-[32px] bg-white px-5 md:mx-16 mb-[65px]">
+      <div className="mt-[32px] bg-white px-5 md:mx-16 lg:mx-0 mb-[65px] md:rounded-lg">
         <h1 className="font-bold text-xl pt-[24px]">Upload Preview Video</h1>
         <hr className="bg-[#55555733] mt-[12px] mb-[18px]" />
         <Files
